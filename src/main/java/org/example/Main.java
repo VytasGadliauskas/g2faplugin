6cf4005a91b637f11af53a163d1812e9d9bd9e5b
Vytas Gadliauskas <vytasgadliauskas@gmail.com>
2023-06-07 02:54:52 +0300

    first code release

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,14 +1,10 @@
 package org.example;
 
-import com.google.zxing.BarcodeFormat;
-import com.google.zxing.MultiFormatWriter;
 import com.google.zxing.WriterException;
-
-import com.google.zxing.common.BitMatrix;
 import de.taimos.totp.TOTP;
+import io.github.shashankn.qrterminal.QRCode;
 import org.apache.commons.codec.binary.Base32;
 import org.apache.commons.codec.binary.Hex;
-
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
 import java.net.URLEncoder;
@@ -20,11 +16,10 @@ public class Main {
         String secretKey = null;
         if (args.length > 0) {
             secretKey = ConfigFile.GetKeyFromConfigFile();
-            //  String secretKey = "IPRPPPQQDYUNCVPKDT6RDTJYU2HBFH6U";
             if (args[0].equals("--key")&&(args.length == 3)) {
                 ConfigFile.PutKeyToConfigFile(generateSecretKey());
                 secretKey = ConfigFile.GetKeyFromConfigFile();
-                System.out.println(createQRCode(getGoogleAuthenticatorBarCode(secretKey,args[1],args[2]),10,10));
+                System.out.println(QRCode.from(getGoogleAuthenticatorBarCode(secretKey,args[1],args[2])).generate());
                 System.out.println("");
                 System.out.println("Google 2FA CODE: "+secretKey);
                 System.exit(0);
@@ -32,18 +27,19 @@ public class Main {
                 System.out.println("---------------------------------------------------------------------------------------------");
                 System.out.println("--help             : display help");
                 System.out.println("--key email name   : generate new QR-code");
+                System.out.println("  for OpenVpn: ");
+                System.out.println("java -jar g2faplugin.jar username='any' password='2fa code' ");
                 System.out.println("");
-                System.out.println("1. generate google 2fa code:  java -jar g2faplugin.jar --key email isuer ");
+                System.out.println("1. generate google 2fa code:  java -jar g2faplugin.jar --key email name ");
                 System.out.println("2. install on android Google authentificator  ");
                 System.out.println("https://play.google.com/store/apps/details?id=com.google.android.apps.authenticator2&hl=en_US");
-                System.out.println("3. usage: java -jar g2faplugin.jar username='any' password='2fa code' ");
+                System.out.println("3. usage in OpenVpn: java -jar g2faplugin.jar username='any' password='2fa code' ");
                 System.out.println("");
                 System.out.println("2023.06 author: vytasgadliauskas@gmail.com");
                 System.out.println("---------------------------------------------------------------------------------------------");
             } else {
                 String code = getTOTPCode(secretKey);
-                if (!code.equals(null)) {
-                    // System.out.println(code);
+                if (code != null) {
                     HashMap<String, String> params = new HashMap<>();
                     for (int i = 0; i < args.length; i++) {
                         if (args[i].contains("=")) {
@@ -55,14 +51,23 @@ public class Main {
                         if (!params.get("password").equals(null)) {
                             if (params.get("password").equals(code)) {
                                 System.out.println("1");
+                                System.exit(0);
                             } else {
                                 System.out.println("0");
+                                System.exit(0);
                             }
                         }
                     }
                 }
+                System.out.println("try with --help");
+                System.out.println("0");
+                System.exit(0);
             }
+            System.out.println("0");
+            System.exit(0);
         }
+        System.out.println("try with --help");
+        System.out.println("0");
     }
 
     public static String generateSecretKey() {
@@ -75,9 +80,12 @@ public class Main {
 
     public static String getTOTPCode(String secretKey) {
         Base32 base32 = new Base32();
-        byte[] bytes = base32.decode(secretKey);
-        String hexKey = Hex.encodeHexString(bytes);
-        return TOTP.getOTP(hexKey);
+        if (secretKey != null) {
+            byte[] bytes = base32.decode(secretKey);
+            String hexKey = Hex.encodeHexString(bytes);
+            return TOTP.getOTP(hexKey);
+        }
+        return null;
     }
 
     public static String getGoogleAuthenticatorBarCode(String secretKey, String account, String issuer) {
@@ -90,13 +98,4 @@ public class Main {
             throw new IllegalStateException(e);
         }
     }
-
-    public static String createQRCode(String barCodeData, int height, int width) throws WriterException {
-        BitMatrix matrix = new MultiFormatWriter().encode(barCodeData, BarcodeFormat.QR_CODE, width, height);
-        return matrix.toString();
-    }
-
-
 }
-
-
